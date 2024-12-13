b6421d0cbfc1c93f597d55a64480c4e7d820cd63
TeslaWillow <hectorjosue.caballero@gmail.com>
2024-12-12 23:50:07 -0600

    initial SPA

---
+++ src/app/pages/about-page/about-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './about-page.component.html',
+})
+export default class PricingPageComponent {
+
+}
---
+++ src/app/pages/pricing-page/pricing-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pricing-page',
+  imports: [],
+  templateUrl: './pricing-page.component.html',
+})
+export default class AboutPageComponent {
+
+}
