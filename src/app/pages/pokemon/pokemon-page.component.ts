3dad2c8765ec8df514241f63e53ca723a62b5922
TeslaWillow <hectorjosue.caballero@gmail.com>
2025-01-21 20:43:12 -0600

    read query parameter

--- src/app/pages/pokemon/pokemon-page.component.ts
+++ src/app/pages/pokemon/pokemon-page.component.ts
@@ -2,6 +2,11 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { PokemonListComponent } from '../../pokemons/components/pokemon-list/pokemon-list.component';
 import { PokemonsService } from '../../pokemons/services/pokemons.service';
 import { SimplePokemon } from '../../pokemons/interfaces';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map, tap } from 'rxjs';
+import { Title } from '@angular/platform-browser';
 
 
 @Component({
@@ -20,7 +25,18 @@ export default class PokemonPageComponent implements OnInit {
   // });
 
   private _pokemon = inject(PokemonsService);
+  private _router = inject(Router);
+  private _title = inject(Title);
   public pokemons  = signal<SimplePokemon[]>([]);
+
+  private _route = inject(ActivatedRoute);
+  public currentPage = toSignal<number>(
+    this._route.queryParamMap.pipe(
+      map( params => params.get('page') ?? '1' ),
+      map( page => ( isNaN(+page) ? 1 : +page )),
+      map( page => ( Math.max(1, page) )),
+    )
+  );
 
   constructor(){}
 
@@ -28,14 +44,24 @@ export default class PokemonPageComponent implements OnInit {
     // setTimeout(() => {
     //   this.isLoading.set(false);
     // }, 1000);
+
+    console.log( this.currentPage() );
     this.loadPokemons();
   }
 
   public loadPokemons( page = 0 ): void {
-    this._pokemon.loadPage(page).subscribe({
-      next: (pokemons) => { this.pokemons.set(pokemons); },
-      error: (e) => { console.error(e); }
-    });
+
+    const pageToLoad = this.currentPage()! + page;
+
+    this._pokemon.loadPage(pageToLoad)
+      .pipe(
+        tap(() => this._router.navigate([], {queryParams: { page: pageToLoad }}) ),
+        tap(() => this._title.setTitle(`Pokemon SSR - Page ${this.currentPage()}`))
+      )
+      .subscribe({
+        next: (pokemons) => { this.pokemons.set(pokemons); },
+        error: (e) => { console.error(e); }
+      });
   }
 
   // ngOnDestroy(): void {
