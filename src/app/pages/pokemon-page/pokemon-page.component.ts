70554ab5736e07736da8b3f6843ccce22f527a94
TeslaWillow <hectorjosue.caballero@gmail.com>
2025-01-21 23:08:02 -0600

    feat: enhance SEO by updating page title and meta tags for individual Pokémon

--- src/app/pages/pokemon-page/pokemon-page.component.ts
+++ src/app/pages/pokemon-page/pokemon-page.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { Pokemon } from '../../pokemons/interfaces';
 import { PokemonsService } from '../../pokemons/services/pokemons.service';
 import { ActivatedRoute } from '@angular/router';
+import { tap } from 'rxjs';
+import { Meta, Title } from '@angular/platform-browser';
 
 
 @Component({
@@ -14,6 +16,8 @@ export default class PokemonPageComponent implements OnInit {
 
   private _pokemonService = inject(PokemonsService);
   private _route = inject(ActivatedRoute);
+  private _title = inject(Title);
+  private _meta = inject(Meta);
 
   public pokemon = signal<Pokemon | null>(null);
 
@@ -22,7 +26,17 @@ export default class PokemonPageComponent implements OnInit {
 
     if(!id) return;
 
-    this._pokemonService.loadPokemon(id).subscribe(
+    this._pokemonService.loadPokemon(id)
+      .pipe(
+        tap(({ name, id }) => {
+          this._title.setTitle(`Pokemon: #${id} - ${name}`);
+          this._meta.updateTag({ name: 'description', content: `Página dePokemon: ${name}` });
+          this._meta.updateTag({ name: 'og:title', content: `Pokemon: ${name}` });
+          this._meta.updateTag({ name: 'og:description', content: `Página dePokemon: ${name}` });
+          this._meta.updateTag({ name: 'og:image', content: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png` });
+        })
+      )
+      .subscribe(
       (pokemon) => {
         this.pokemon.set(pokemon);
       }
