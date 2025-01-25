7132d8b5b3a3dcd3bdcf2e17f17380c856a7c2df
TeslaWillow <hectorjosue.caballero@gmail.com>
2025-01-25 00:10:07 -0600

    better test

--- src/app/pokemons/components/pokemon-card/pokemon-card.component.spec.ts
+++ src/app/pokemons/components/pokemon-card/pokemon-card.component.spec.ts
@@ -41,15 +41,22 @@ describe('Pokemon card component', () => {
 
   it('should render the pokemon name and image correctly', () => {
     const img = compiled.querySelector('img');
-    const pokemonName = compiled.querySelector('h2');
+    expect(img).toBeDefined();
 
-    expect(img).toBeTruthy();
+    expect(img?.src).toBe(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${mockPokemon.id}.png`);
+
+    const pokemonName = compiled.querySelector('h2');
     expect(pokemonName?.textContent?.trim()).toBe('bulbasaur');
   });
 
   it('should have the proper ng-reflect-router-link', () => {
     const routerLink = compiled.querySelector('.cursor-pointer');
     expect(routerLink?.getAttribute('ng-reflect-router-link')).toBeTruthy();
+
+    expect(
+      routerLink?.attributes.getNamedItem('ng-reflect-router-link')?.value
+    ).toBe(`/pokemon,${mockPokemon.name}`);
+
   });
 
 });
