7391ec1d26b523f0af4893df4fea333598528f8a
TeslaWillow <hectorjosue.caballero@gmail.com>
2025-01-31 22:53:44 -0600

    mock http request

--- src/app/pokemons/services/pokemons.service.spec.ts
+++ src/app/pokemons/services/pokemons.service.spec.ts
@@ -2,8 +2,29 @@
 import { TestBed } from "@angular/core/testing";
 import { PokemonsService } from "./pokemons.service";
 import { provideHttpClient } from "@angular/common/http";
-import { provideHttpClientTesting } from "@angular/common/http/testing";
-import { SimplePokemon } from "../interfaces";
+import { HttpTestingController, provideHttpClientTesting } from "@angular/common/http/testing";
+import { PokeAPIResponce, SimplePokemon } from "../interfaces";
+
+
+const mockPokeApiResponce: PokeAPIResponce = {
+  "count": 1304,
+  "next": "https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20",
+  "previous": null,
+  "results": [
+      {
+          "name": "bulbasaur",
+          "url": "https://pokeapi.co/api/v2/pokemon/1/"
+      },
+      {
+          "name": "ivysaur",
+          "url": "https://pokeapi.co/api/v2/pokemon/2/"
+      },
+      {
+          "name": "venusaur",
+          "url": "https://pokeapi.co/api/v2/pokemon/3/"
+      },
+  ]
+};
 
 const expectedPokemons: SimplePokemon[] = [
   {
@@ -28,6 +49,7 @@ const mockPokemon = {
 describe('PokemonsService', () => {
 
   let service: PokemonsService;
+  let httpMock: HttpTestingController;
 
   beforeEach(()=>{
     TestBed.configureTestingModule({
@@ -37,10 +59,30 @@ describe('PokemonsService', () => {
       ]
     });
     service = TestBed.inject(PokemonsService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify(); // Close all http connections
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should load a page of SimplePokemons', () => {
+    service.loadPage(1).subscribe({
+      next: (pokemons) => {
+        expect(pokemons).toEqual(expectedPokemons);
+      }
+    });
+    const url = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20`;
+
+    const req = httpMock.expectOne(url);
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockPokeApiResponce); // Send manuany
   });
 
 });
